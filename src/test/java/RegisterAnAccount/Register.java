7303fd48e4cfbdb3dfd6da7540b009997ed287a8
Mohamed-Ashraf25 <81617587+Mohamed-Ashraf25@users.noreply.github.com>
2024-08-22 21:13:13 +0300

    First Step

--- src/test/java/RegisterAnAccount/Register.java
+++ src/test/java/RegisterAnAccount/Register.java
@@ -1,16 +1,18 @@
 package RegisterAnAccount;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.*;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class Register {
     WebDriver Driver;
@@ -60,11 +62,72 @@ public class Register {
 
     }
     @Test(priority = 2,dependsOnMethods = {"Registeration"})
-public void Login(){
+    public void PopUpAlet(){
 
+        WebElement AlertBtn=Driver.findElement(By.id(""));
+        AlertBtn.click();
+        Alert alert=Driver.switchTo().alert();
+        String Text=alert.getText();
+        Assert.assertEquals(Text,"");
+        alert.sendKeys("");
+        alert.accept();
+        alert.dismiss();
 
 
     }
+    @Test(priority = 3,enabled = false)
+    public  void TestMultipleWindow(){
+        String CurrnetWindowId=Driver.getWindowHandle();
+        for(String WindowId:Driver.getWindowHandles()){
+
+            String Title=Driver.switchTo().window(WindowId).getTitle();
+            if (Title.equalsIgnoreCase("")){
+                Assert.assertEquals("",Driver.getTitle());
+                Driver.close();
+                break;
+            }
+        }
+        Driver.switchTo().window(CurrnetWindowId);
+    }
+
+    @Test(priority = 4,enabled = false)
+    public  void  TestCookies(){
+        Cookie StokeCookie=Driver.manage().getCookieNamed("");
+        Assert.assertEquals(null,"");
+        Select Slc=new Select(Driver);
+        Slc.deselectByVisibleText("");
+        StokeCookie=Driver.manage().getCookieNamed("");
+        Assert.assertEquals("",StokeCookie.getValue());
+        Set<Cookie> CookiesGroup=Driver.manage().getCookies();
+        Iterator<Cookie> itr=CookiesGroup.iterator();
+        while (itr.hasNext()){
+
+            Cookie Cookiee=itr.next();
+            System.out.println(Cookiee.getName());
+        }
+    }
+
+    @Test(priority = 5,enabled = false)
+    public void TestSynchonizationtime(){
+        Thread.sleep(1000);   //Threed Sleep
+        Driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);  //implicit wait
+        WebDriverWait ExplicitWait=new WebDriverWait(Driver,1000);//Explicit Wait "Used to Wait Until Condition "
+        ExplicitWait.until(ExpectedConditions.titleContains(""));
+
+        Wait<WebDriver> FluentWait=new FluentWait<WebDriver>(Driver).withTimeout(20,TimeUnit.SECONDS)
+                .pollingEvery(2,TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class);
+
+        WebElement Message=FluentWait.until(new Function<WebDriver, WebElement>() {
+            @Override
+            public WebElement apply(WebDriver webDriver) {
+                return webDriver.findElement(By.id(""));
+            }
+        })
+
+    }
+
+
     @AfterTest
     public void TearDown() {
         //Driver.quit();
